5b2678666170811267a4e9ba29e85efb1fabf71b
Aung Kyaw Oo <aung.kyawoo@seattleconsultingmyanmar.com>
2020-07-06 10:56:56 +0630

    gg

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <h1>NI Hao!</h1>
     <about></about>
   </div>
@@ -9,12 +8,10 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 import about from '@/views/About.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
     about
   }
 }
